ea877b28b07416ed423cbdcabd53a577703b4629
Your Name <elnuraliyev1@gmail.com>
2022-06-01 23:42:49 -0500

    update

--- penguins-app.py
+++ penguins-app.py
@@ -8,11 +8,10 @@ from sklearn.ensemble import RandomForestClassifier
 st.write("""
 # Penguin Prediction App
 This app predicts the **Palmer Penguin** species!
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+Data obtained from the [palmerpenguins library] in R by Allison Horst.
 """)
 
 st.sidebar.header('User Input Features')
 
 st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-""")
+[Example CSV input file]""")
